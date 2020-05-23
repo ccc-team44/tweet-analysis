f616564a3853345ef9b00dee80401cd4f02b97c4
handongl <55689507+handongl@users.noreply.github.com>
2020-05-23 15:18:49 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import time
 
 import language_senario
 import retweet_senario
+import morrison_by_month
+
 while True:
-    language_senario.main();
-    retweet_senario.main();
-    time.sleep(60000) #1 hour
+    try:
+        language_senario.main()
+    except:
+        print('error in language senario')
+    try:
+        retweet_senario.main()
+    except:
+        print('error in retweet senario')
+    try:
+        morrison_by_month.main()
+    except:
+        print('error in morrison senario')
+    time.sleep(60000)  # 1 hour
